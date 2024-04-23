a440214815ca81a5eb9822e294b5788df7c83edb
Dominique Gajardo <dominique.gajardoap@gmail.com>
2024-04-22 22:32:16 -0400

    Subiendo desadio a github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Navigation from "./components/Navigation"
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route />
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
